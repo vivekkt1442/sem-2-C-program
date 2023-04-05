db3b2025b83912837fccad712fe3a50bca12e34b
Vivek <121384293+vivekkt1442@users.noreply.github.com>
2023-04-06 00:18:40 +0530

    Add files via upload

---
+++ exp1_3.cpp
@@ -0,0 +1,15 @@
+// Write A c++ Program To Read And Display Multiple Value In A SIngle I/O statement.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "C++ is better than C."
+         << "My Name Is VIVEKKT." << endl;
+
+    cin >> a >> b;
+
+    cout << a << b;
+    return 0;
+}
